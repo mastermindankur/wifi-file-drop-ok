9e00bf93bdca868720efa347a5f8b52ec58b5a1c
ankur khera <mastermindankur@gmail.com>
2025-07-26 09:17:44 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,21 +7,20 @@ let db: any;
 
 const initializeFirebase = () => {
     if (typeof window !== 'undefined') {
-        if (!getApps().length) {
-            const firebaseConfig = {
-              projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-              appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-              storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-              apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-              authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-              measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-              messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-              databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-            };
+        const firebaseConfig = {
+            projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+            appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+            storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+            apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+            authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+            measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+            messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+            databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+        };
 
-            if (!firebaseConfig.projectId || !firebaseConfig.apiKey) {
-                console.error("Firebase config is not set up correctly. Please check your .env.local file");
-                return;
+        if (!getApps().length) {
+            if (!firebaseConfig.projectId || !firebaseConfig.apiKey || !firebaseConfig.databaseURL) {
+                throw new Error("Firebase config is not set up correctly. Please make sure all NEXT_PUBLIC_FIREBASE_ environment variables are set in your .env file.");
             }
             app = initializeApp(firebaseConfig);
             db = getDatabase(app);
